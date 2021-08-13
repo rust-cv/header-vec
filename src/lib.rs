f6753cf9e0b7b4bcaa3888d385331e739b15b041
mako <marcus.yass@gmail.com>
2021-08-13 22:29:44 +1200

    removed a trailing space from the doc example

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@ struct HeaderVecHeader<H> {
 /// let mut hv = HeaderVec::<OurHeaderType, char>::new(h);
 /// hv.push('x');
 /// hv.push('z');
-/// 
+///
 /// println!("HeaderVec itself consists solely of a pointer, it's only {} bytes big.", size_of_val(&hv));
 /// println!("All of the data, like our header, {:?}, and the length of the vector: {}, resides on the other side of the pointer.", &*hv, hv.len());
 /// ```
